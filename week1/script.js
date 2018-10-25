49f78c7af6007e6691672bdd4826c06397f1b878
akssaeed <qs2007@gmail.com>
2018-10-25 21:35:02 +0200

    update using let

--- week1/script.js
+++ week1/script.js
@@ -1,13 +1,13 @@
 // Home Work Part 1
-var Lang = "in English";
+let Lang = "in English";
 console.log("Hello World "+ Lang); //English
-var Lang = "in Nederlands";
+Lang = "in Nederlands";
 console.log("Hallo wereld "+ Lang); //Dutch
-var Lang = "en Français";
+Lang = "en Français";
 console.log("Bonjour le monde "+ Lang); //Frensh
-var Lang = "in italiano";
+Lang = "in italiano";
 console.log("Ciao mondo " + Lang); //Italian
-var Lang = "en español";
+Lang = "en español";
 console.log("Hola mundo " + Lang); //Spanish
 
 /* Home Work Part 2
@@ -15,35 +15,35 @@ console.log('I'm awesome'); have an error and should be like following */
 console.log('I\'m awesome');
 
 //part 3
-var x ;
+let x ;
  console.log('the value of my variable x will be: undefined');
  console.log(x);
  
-var x = 5;
+x = 5;
  console.log('the value of my variable x will be: number > 5');
  console.log(x);
  
  //part 4
- var y ;
+let y ;
  console.log('the value of my variable y will be: undefined');
  console.log(y);
  
- var y = "Just I add this words to test";
+y = "Just I add this words to test";
  console.log('the value of my variable y will be: string > Just I add this words to test');
  console.log(y);
 
 //part 5 
- var z = 7.25;
+let z = 7.25;
  console.log(z);
  
- var a = Math.round(z);
+let a = Math.round(z);
  console.log(a);
  
- var n = Math.max(a, z);
+let n = Math.max(a, z);
  console.log(n);
  
  //part 6
- var myFavoriteAnimals = [];
+let myFavoriteAnimals = [];
  console.log("the value of my Arry will be: Empty or null");
  console.log(myFavoriteAnimals);
  
@@ -54,8 +54,8 @@ var x = 5;
  console.log(myFavoriteAnimals);
  
  //part 7
- let myString = "this is a test";
- var l = myString.length;
+let myString = "this is a test";
+let l = myString.length;
  console.log('The length of my string is '+ l + ' letters');
  
  //part 8
@@ -107,36 +107,36 @@ var x = 5;
 	
 //part 9
 
-var x = 7;
-var x = x % 3;
+x = 7;
+x = x % 3;
  console.log('The value of my variable x is: 1');
  console.log(x);
  
-var x = 9;
-var x = x % 3;
+x = 9;
+x = x % 3;
  console.log('The value of my variable x is: 0');
  console.log(x);
  
-var x = 15;
-var x = x % 4;
+x = 15;
+x = x % 4;
  console.log('The value of my variable x is: 3');
  console.log(x);
  
-var x = 150;
-var x = x % 8;
+x = 150;
+x = x % 8;
  console.log('The value of my variable x is: 6');
  console.log(x);
  
 //part 10
-var myTestArray = [];
+let myTestArray = [];
 myTestArray.push("String",5);
 console.log(myTestArray);
 console.log(typeof myTestArray[0]);
 console.log(typeof myTestArray[1]);
 
 
-var x = 6/0;
-var y = 10/0;
+x = 6/0;
+y = 10/0;
 console.log(x);
 console.log(y);
 console.log(typeof x);
